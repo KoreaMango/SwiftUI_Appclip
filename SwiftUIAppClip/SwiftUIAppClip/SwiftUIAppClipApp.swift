1be64bd6f799a6a11e33c87b4ec2fa97260fb5ed
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-07-31 14:23:24 +0900

    proj init

---
+++ SwiftUIAppClip/SwiftUIAppClip/SwiftUIAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppClipApp.swift
+//  SwiftUIAppClip
+//
+//  Created by 강민규 on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

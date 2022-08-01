be7f6d53c3b2f1d83913443b8ee22fd68126471a
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-08-01 11:52:41 +0900

    Clip Add & foldering

---
+++ SwiftUIAppClip/Clip/ClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipApp.swift
+//  Clip
+//
+//  Created by 강민규 on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct ClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
